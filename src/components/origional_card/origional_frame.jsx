cc995d79b479e98ac0cc210dc455cccfa0a4b1fd
unknown <ajfry203@gmail.com>
2023-06-13 15:02:01 +1200

    Start of frame

---
+++ src/components/origional_card/origional_frame.jsx
@@ -0,0 +1,25 @@
+import './origional_frame.css;'
+class cardframe extends React.Component {
+    render() {
+      return <div className="outer-frame">
+        <div className="inner-frame">
+            <div className="headerbox">
+                <h2 className="headerbox">
+                    {this.props.cardname}
+                </h2>
+            </div>
+            <img className="cardimage"></img>
+            <div className="tagbox"></div>
+            <div className="textbox"></div>
+            <div className="resbox">
+                <div className="ressingle"></div>
+                <div className="resbreak">|</div>
+                <div className="ressingle"></div>
+                <div className="resbreak">|</div>
+                <div className="ressingle"></div>
+            </div>
+        </div>
+        </div>;
+    }
+  }
+  
